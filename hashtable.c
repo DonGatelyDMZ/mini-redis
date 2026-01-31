2ce7acc88594782fb6efe064149035e4b361c996
DonGatelyDMZ <chrispietrangioli@gmail.com>
2026-02-01 00:07:34 +0100

    started to develop hashtable structure

--- hashtable.c
+++ hashtable.c
@@ -2,3 +2,23 @@
 #include <string.h>
 #include <stdlib.h>
 
+struct Account {
+    char name[40];
+    int id;
+    int hashed_id;
+    struct Account *next;
+};
+
+typedef struct Account account;
+typedef account *accPtr;
+
+void add_account(account * last_account);
+void delete_account(char *name);
+int hash_id(char *name);
+
+account arr[10];
+
+int main(void) {
+    return 0;
+}
+
