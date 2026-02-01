252cbd746f6e1d5a81f46bb290752230f0420c69
DonGatelyDMZ <chrispietrangioli@gmail.com>
2026-02-01 21:29:24 +0100

    aggiornato hashtable

--- hashtable.c
+++ hashtable.c
@@ -10,15 +10,36 @@ struct Account {
 };
 
 typedef struct Account account;
-typedef account *accPtr;
 
-void add_account(account * last_account);
-void delete_account(char *name);
-int hash_id(char *name);
+void add_account();
+void delete_account(char **name);
+int hash_id(char *name); //
+void refresh_last_pointer(void); //refreshed last pointer for each array position
 
-account arr[10];
+account *arr[10];
+account *last_accounts[10] = {NULL};
 
 int main(void) {
     return 0;
 }
 
+void add_account() {
+    char temp_name[40];
+    printf("Insert name: ");
+    scanf("%s", temp_name);
+    int hash = hash_id(temp_name);
+    account *new_account = last_accounts[hash];
+    if (new_account == NULL) {
+        new_account = malloc(sizeof(account));
+        strcpy(&(new_account->name), temp_name);
+        new_account->hashed_id = hash;
+        new_account->next = NULL;
+    }
+    else {
+        ->next = malloc(sizeof(account))
+    }
+}
+
+void refresh_last_pointer(void) {
+
+}
